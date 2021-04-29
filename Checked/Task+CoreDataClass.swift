60e6ee3898ce136b214325aa2f411d06e4827a7b
Larry N <larryn35@gmail.com>
2021-04-29 16:34:37 -0700

    create task model

---
+++ Checked/Task+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Task+CoreDataClass.swift
+//  Checked
+//
+//  Created by Larry N on 4/29/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Task)
+public class Task: NSManagedObject {
+
+}
